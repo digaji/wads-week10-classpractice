1a6adeccf34de350c4d9dfdb2dbccfa802b66efe
Jason J. W <71363607+digaji@users.noreply.github.com>
2022-05-20 01:42:00 +0700

    Fixed some styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,69 +1,79 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-      ],
+      items: [],
       inputField: "",
-      initialCall: false
-    }
+      initialCall: false,
+    };
     this.url = "http://localhost:3001/hero";
   }
 
   async getAllHero() {
-    const {data} = await axios.get(this.url);
-    this.setState(state => ({items: data.map(dat => ({_id: dat._id, heroname: dat.heroname}))}));
+    const { data } = await axios.get(this.url);
+    this.setState((state) => ({
+      items: data.map((dat) => ({ _id: dat._id, heroname: dat.heroname })),
+    }));
   }
 
   async componentDidMount() {
     this.getAllHero();
   }
-  
 
   render() {
-    const items = 
-    this.state.items.map((item, id) => (
-    <li 
-      key={item._id} 
-      onClick={
-        (e) => {
-        axios.delete(this.url + `/${item._id}`).then((res) => {
-          this.setState(state => ({
-            items: state.items.filter((nitem, i) => (nitem._id !== item._id))
-          }));
-          console.log(`Successfully deleted ${res.data} heros`);
-        })
-        }
-      }
-    >{item.heroname}</li>));
+    const items = this.state.items.map((item, id) => (
+      <li
+        key={item._id}
+        onClick={(e) => {
+          axios.delete(this.url + `/${item._id}`).then((res) => {
+            this.setState((state) => ({
+              items: state.items.filter((nitem, i) => nitem._id !== item._id),
+            }));
+            console.log(`Successfully deleted ${res.data} heros`);
+          });
+        }}
+      >
+        {item.heroname}
+      </li>
+    ));
     return (
       <div className="App">
         <h1>Hero Keeper</h1>
-        <p>List all your favorite heros here</p>
+        <p>List all your favorite heroes here</p>
         <ul>{items}</ul>
+        <p>
+          <strong>Hero name down below</strong>
+        </p>
         <input
           onChange={(e) => {
-           this.setState({inputField: e.target.value})
-          }} 
+            this.setState({ inputField: e.target.value });
+          }}
           onKeyPress={async (e) => {
-            if ((e.key === "Enter") && (this.state.inputField !== "")) {
+            if (e.key === "Enter" && this.state.inputField !== "") {
               const heroname = this.state.inputField;
-              this.setState({inputField: ""});
+              this.setState({ inputField: "" });
               const res = await axios.post(this.url, {
                 heroname: heroname,
-                realname: ""
+                realname: "",
               });
-              this.setState(state => ({items: state.items.concat({_id: res.data, heroname: heroname})}));
+              this.setState((state) => ({
+                items: state.items.concat({
+                  _id: res.data,
+                  heroname: heroname,
+                }),
+              }));
             }
           }}
-          value={this.state.inputField}></input>
+          value={this.state.inputField}
+          type="text"
+          placeholder="Hero name"
+        ></input>
       </div>
-    )
+    );
   }
 }
 
